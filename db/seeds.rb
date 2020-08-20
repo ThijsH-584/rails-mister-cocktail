1e07a3e9dc638f0cac09d60f74675cc0b7bc0530
Thijs Hakkenberg <thijshakkenberg@hotmail.com>
2020-08-20 18:16:58 +0200

    Mister Cocktail project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+puts "Wiped Instances"
+
+INGREDIENTS = ["lemon", "ice", "mint leaves", "orange juice"]
+
+INGREDIENTS.each do |ingredient|
+	Ingredient.create(name: ingredient)
+end
+
+COCKTAILS = ["Mint Julep", "Gonzalo Morning Brew", "Sunny Day", "Leaves From Steves"]
+
+COCKTAILS.each do |cocktail|
+	Cocktail.create(name: cocktail)
+end
+
+DOSES = ["1 cl", "4 cl", "4 leafes", "2 cl"]
+
+
+DOSES.each do |dose|
+	new_dose = Dose.new(description: dose)
+	new_dose.ingredient = Ingredient.all.sample
+	new_dose.cocktail = Cocktail.all.sample
+	new_dose.save
+end
+
+puts "#{Ingredient.count} ingredients created"
+puts "#{Cocktail.count} cocktails created"
+puts "#{Dose.count} doses created"
